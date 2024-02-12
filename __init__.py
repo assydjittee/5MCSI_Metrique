8f8e84eaa6c0ffaa0b5fc764299ad34ed3b821cb
assydjittee <81174855+assydjittee@users.noreply.github.com>
2024-02-12 17:15:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,9 +7,10 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)       
 
+
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def contac():
+    return render_template('contact.html') 
                                                                                                                                        
 @app.route('/')
 def hello_world():
