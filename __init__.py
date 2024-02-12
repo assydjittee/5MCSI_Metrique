9b28f50edeffd9e9472059769a31dd6bada29160
assydjittee <81174855+assydjittee@users.noreply.github.com>
2024-02-12 17:18:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,16 +5,17 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)       
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 @app.route("/contact/")
-def contac():
-    return render_template('contact.html') 
-                                                                                                                                       
-@app.route('/')
+def contact():
+    return render_template("contact.html")
+  @app.route('/')
 def hello_world():
     return render_template('hello.html') #comment2
-  
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
+                                                                                                                                       
